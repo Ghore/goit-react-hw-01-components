b50052e07e4922b2146cb9dad2ce172c901b91ab
Ghore <ghoreua@gmail.com>
2020-05-04 15:20:59 +0300

    deploy

---
+++ build/precache-manifest.61dc18ddac133e71bea0dd2d39a3a36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a028a06cb15f94e0b18a4677f1f0cfdf",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "2a231fcf667a20f6cdcd",
+    "url": "/goit-react-hw-01-components/static/css/main.998ec124.chunk.css"
+  },
+  {
+    "revision": "c96098661d2e9999fdb1",
+    "url": "/goit-react-hw-01-components/static/js/2.5a20dbc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.5a20dbc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a231fcf667a20f6cdcd",
+    "url": "/goit-react-hw-01-components/static/js/main.57113d67.chunk.js"
+  },
+  {
+    "revision": "4c937360e5260d83309f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.0939332a.js"
+  }
+]);
